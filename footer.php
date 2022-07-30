3a1911e0e6c5063bd3f07632acc7725c57e018f2
AmeerSuhail-M <73430970+AmeerSuhail-M@users.noreply.github.com>
2022-07-30 11:00:18 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,108 @@
+	
+	<!-- Start Contact info -->
+	<!-- <div class="contact-imfo-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 arrow-right">
+					<i class="fa fa-volume-control-phone"></i>
+					<div class="overflow-hidden">
+						<h4>Phone</h4>
+						<p class="lead">
+							080-67854932
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4 arrow-right">
+					<i class="fa fa-envelope"></i>
+					<div class="overflow-hidden">
+						<h4>Email</h4>
+						<p class="lead">
+							yumyumtree@gmail.com
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<i class="fa fa-map-marker"></i>
+					<div class="overflow-hidden">
+						<h4>Location</h4>
+						<p class="lead">
+							Bangalore
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div> -->
+	<!-- End Contact info -->
+	
+	<!-- Start Footer -->
+	<footer class="footer-area bg-f">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+					<h3>About Us</h3>
+					<p>We prepare the Best Food from all organic ingredients and are serving  our happy customers from past 10 years .</p>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Useful Links</h3>
+					<div class="subscribe_form">
+						
+						<a href="/maher_restaurent/index.php" style="color:#FF0000;">Home</a><br>
+						<a href="/maher_restaurent/contact.php" style="color:#FF0000;">Contact Us</a><br>
+						<a href="/maher_restaurent/menu.php" style="color:#FF0000;">Food Menu</a><br>
+						<a href="/maher_restaurent/gallery.php" style="color:#FF0000;">Gallery</a><br>
+						
+						
+					</div>
+					<ul class="list-inline f-social">
+						<li class="list-inline-item"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Contact information</h3>
+					<p class="lead"> Bangalore</p>
+					<p class="lead"><a href="#">080-67854932</a></p>
+					<p><a href="#"> yumyumtree@gmail.com</a></p>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h3>Opening hours</h3>
+					<p><span class="text-color">Monday-Friday: </span> 8:Am - 9PM</p>
+					<p><span class="text-color">Saturday-Sunday :</span> 8:Am - 12PM</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<p class="company-name"> &copy; 2021 <a href="#">YumYumTree Restaurant</a> 
+					<a href="">Developed By :  Ameer Suhail M</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</footer>
+	<!-- End Footer -->
+	
+	<a href="#" id="back-to-top" title="Back to top" style="display: none;"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></a>
+
+	<!-- ALL JS FILES -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="js/jquery.superslides.min.js"></script>
+	<script src="js/images-loded.min.js"></script>
+	<script src="js/isotope.min.js"></script>
+	<script src="js/baguetteBox.min.js"></script>
+	<script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+</html>
